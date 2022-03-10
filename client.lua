c2eac3b58f55e7a9a24b4bbaa8d667475c73abd0
Puntzi <70822492+Puntzi@users.noreply.github.com>
2022-03-10 16:28:51 +0100

    Better performance

--- client.lua
+++ client.lua
@@ -26,56 +26,50 @@ end)
 
 
 Citizen.CreateThread(function()
-	while true do
-		Wait(10)
+    while true do
+        local player = PlayerPedId()
+        local playerCoords = GetEntityCoords(player)
+        local armor = GetPedArmour(player)
+        local sleep = 500
 
-local player = PlayerPedId()
-local playerCoords = GetEntityCoords(player)
-
-
-
-local armor = GetPedArmour(player)
-
-
-for k,v in ipairs(Config.Zone) do
-    local distance = #(playerCoords - v)
-    if PlayerData.job ~= nil and PlayerData.job.name == Config.job or PlayerData.job ~= nil and PlayerData.job.name == Config.job2 then 
-if distance <= 3 then
-    ESX.ShowFloatingHelpNotification(_U('press'),vector3(v))
-    if distance <= 1.5 and  IsControlPressed(0, 38) then
-      
-        
-        TriggerEvent("mythic_progbar:client:progress", {
-            name = "vest-wearing",
-            duration = 10000,
-            label = _U('armor_wearing'),
-            useWhileDead = false,
-            canCancel = true,
-            controlDisables = {
-                disableMovement = true,
-                disableCarMovement = true,
-                disableMouse = false,
-                disableCombat = true,
-            },
-            animation = {
-                animDict = "clothingshirt",
-                anim = "try_shirt_positive_d",
-            },
-        }, function(status)
-            if not status then
-                if armor >= Config.ArmorAmount then
-                    ESX.ShowNotification(_U('armor_max'))
-            else
-               SetPedArmour(player, Config.ArmorAmount)
+        if PlayerData.job ~= nil and PlayerData.job.name == Config.job or PlayerData.job ~= nil and PlayerData.job.name == Config.job2 then 
+            for k,v in pairs(Config.Zone) do
+                local distance = #(playerCoords - v)
+                if distance <= 3 then
+                    sleep = 0
+                    ESX.ShowFloatingHelpNotification(_U('press'),vector3(v))
+                    if distance <= 1.5 and IsControlPressed(0, 38) then
+                        TriggerEvent("mythic_progbar:client:progress", {
+                            name = "vest-wearing",
+                            duration = 10000,
+                            label = _U('armor_wearing'),
+                            useWhileDead = false,
+                            canCancel = true,
+                            controlDisables = {
+                                disableMovement = true,
+                                disableCarMovement = true,
+                                disableMouse = false,
+                                disableCombat = true,
+                            },
+                            animation = {
+                                animDict = "clothingshirt",
+                                anim = "try_shirt_positive_d",
+                            },
+                        }, function(status)
+                            if not status then
+                                if armor >= Config.ArmorAmount then
+                                    ESX.ShowNotification(_U('armor_max'))
+                                else
+                                    SetPedArmour(player, Config.ArmorAmount)
+                                end
+                            end
+                        end)
+                    end
+                end
             end
-            end
-        end)
-
+        end
+        Wait(sleep)
     end
-   end
-end
- end
-end
 end)
 
 
